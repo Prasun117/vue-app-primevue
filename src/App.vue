9e6aaf8a1f319a9837a7baaa0f6c0d2b7b3c2cad
Prasun117 <57390215+Prasun117@users.noreply.github.com>
2023-02-26 19:53:58 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Button
+      class="p-button-secondary"
+      label="Submit"
+      @click="handleClick($event)"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+
+import Button from "primevue/button";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+    Button: Button,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
